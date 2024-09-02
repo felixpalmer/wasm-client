825719fa394caf8be53357b5af271c0387a09df9
Felix Palmer <felixpalmer@gmail.com>
2024-09-02 12:46:06 +0200

    Simple deck test

--- examples/nypd-complaints/src/ChartsPane.tsx
+++ examples/nypd-complaints/src/ChartsPane.tsx
@@ -1,3 +1,6 @@
+import DeckGL from '@deck.gl/react'; 
+import {Map} from 'react-map-gl/maplibre';  
+
 import { DuckDBRow, MDConnection } from "@motherduck/wasm-client";
 import { EventProps } from "@tremor/react";
 import { useCallback, useEffect, useState } from "react";
@@ -5,8 +8,16 @@ import "./ChartsPane.css";
 import { ComplaintTypesForYearChart } from "./ComplaintTypesForYearChart";
 import { ComplaintsByYearChart } from "./ComplaintsByYearChart";
 import { compaintsByYearSql, complaintTypesForYearSql } from "./sql";
+import {ScatterplotLayer} from 'deck.gl';
 
 const noData: readonly DuckDBRow[] = [];
+
+const mapStyle = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
+const INITIAL_VIEW_STATE = {
+  longitude: -74,
+  latitude: 40.7,
+  zoom: 8
+}
 
 export function ChartsPane({ connection }: { connection: MDConnection }) {
   const [complaintsByYearData, setComplaintsByYearData] =
@@ -22,7 +33,8 @@ export function ChartsPane({ connection }: { connection: MDConnection }) {
     async function fetchData() {
       if (connection) {
         const result = await connection.evaluateQuery(compaintsByYearSql);
-        setComplaintsByYearData(result.data.toRows());
+        const rows = result.data.toRows();
+        setComplaintsByYearData(rows);
       }
     }
     fetchData().catch(console.error);
@@ -37,13 +49,21 @@ export function ChartsPane({ connection }: { connection: MDConnection }) {
         const year = Number(value["Year"]);
         const sql = complaintTypesForYearSql(year);
         const result = await connection.evaluateQuery(sql);
-        setComplaintTypesForYearData(result.data.toRows());
+        const rows = result.data.toRows();
+        setComplaintTypesForYearData(rows);
       } else {
         setComplaintTypesForYearData(noData);
       }
     },
     [connection]
   );
+
+
+  const layers = [complaintTypesForYearData && new ScatterplotLayer({
+    data: complaintTypesForYearData,
+    getPosition: d => [d.longitude, d.latitude],
+    radiusMinPixels: 3
+  })];
 
   return complaintsByYearData ? (
     <div id="charts-pane">
@@ -52,9 +72,12 @@ export function ChartsPane({ connection }: { connection: MDConnection }) {
         handleValueChange={handleValueChange}
       />
       {complaintTypesForYearData.length > 0 ? (
-        <ComplaintTypesForYearChart
-          complaintTypesForYearData={complaintTypesForYearData}
-        />
+        <div style={{position: 'relative', background: 'red', height: 300}}>
+          <DeckGL layers={layers} initialViewState={INITIAL_VIEW_STATE} controller={true}>
+            <Map reuseMaps mapStyle={mapStyle} /> 
+          </DeckGL>
+
+        </div>
       ) : null}
     </div>
   ) : (
